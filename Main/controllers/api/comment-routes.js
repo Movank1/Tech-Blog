7001b89cffa1eada1c75290daebf7ddde55085d7
Movank1 <139717943+Movank1@users.noreply.github.com>
2023-12-27 12:21:51 -0500

    updated the API

--- Main/controllers/api/comment-routes.js
+++ Main/controllers/api/comment-routes.js
@@ -1,14 +1,19 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
+    const { body } = req;
+    const userId = req.session.userId;
+
     const newComment = await Comment.create({
-      ...req.body,
-      userId: req.session.userId,
+      body,
+      userId
     });
-    res.json(newComment);
+
+    res.status(201).json(newComment);
   } catch (err) {
     res.status(500).json(err);
   }
--- Main/controllers/dashboard-routes.js
+++ Main/controllers/dashboard-routes.js
@@ -12,6 +12,9 @@ router.get('/', withAuth, async (req, res) => {
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
+    //rendering all the posts for the logged-in user. It uses the Post model to fetch all
+     // the posts associated with the current user's userId.
+
     res.render('all-posts-admin', {
       layout: 'dashboard',
       posts,
@@ -21,11 +24,15 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+// rendering the form to create a new post. It renders the new-post view, along with the dashboard layout.
+
 router.get('/new', withAuth, (req, res) => {
   res.render('new-post', {
     layout: 'dashboard',
   });
 });
+
+// Defining a GET route for '/edit/:id' using the Express.js router.
 
 router.get('/edit/:id', withAuth, async (req, res) => {
   try {
